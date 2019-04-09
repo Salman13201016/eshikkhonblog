e545adea981f51c398c4c5b4b737cf9a77a58605
Salman <salmanmdsultan@gmail.com>
2019-04-10 04:30:40 +0600

    db connection completed

---
+++ database/connect.php
@@ -0,0 +1,15 @@
+<?php
+    $serverame = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "eshikhonblog";
+    
+    $conn = mysqli_connect($serverame,$username,$password,$db);
+    //check connection
+    if(!$conn){
+         die("connection failed:".mysqli_connect_error());
+    }
+    else{
+        die("connection Success:");
+    }
+?>
